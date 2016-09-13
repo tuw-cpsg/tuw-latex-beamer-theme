39e6b274fdaa3cc3c5bda282cc25635ddbc194a5
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2016-09-13 10:23:48 +0200

    change installation (new destination)

--- install_theme.sh
+++ install_theme.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
+##
+# @date 2016-09-13
+# @brief Installs the theme locally.
+# 
+# Local manual installation as described in:
+# http://tex.stackexchange.com/questions/1137/where-do-i-place-my-own-sty-or-cls-files-to-make-them-available-to-all-my-te
+##
 
 THEMEDIR=tuw
 
-for d in $(kpsewhere --expand-var \$TEXMFVAR); do
-    if kpseaccess -w $d; then
-	mkdir -p $d/tex/latex/tuw
-	cp $THEMEDIR/beamerthemeTUW.sty \
-	    $THEMEDIR/TU_Logo.pdf $THEMEDIR/TU_Signet.pdf \
-	    $d/tex/latex/tuw
-	break
-    fi
-done
+# get local tex folder
+TEXDIR=$(kpsewhich -var-value=TEXMFHOME)"/tex/latex/beamer"
+
+# create folder if it does not exist
+mkdir -p "${TEXDIR}"
+
+# install beamer theme
+if kpseaccess -w "${TEXDIR}"; then
+    mkdir -p "${TEXDIR}"/$THEMEDIR
+    cp $THEMEDIR/beamerthemeTUW.sty \
+	$THEMEDIR/TU_Logo.pdf $THEMEDIR/TU_Signet.pdf \
+	"${TEXDIR}"/$THEMEDIR
+    break
+fi
+
+echo "TUW theme installed to ${TEXDIR}/${THEMEDIR}."
