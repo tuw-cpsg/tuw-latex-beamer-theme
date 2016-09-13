37d508ab924b76a129a0dff906c1252b9d398638
Denise Ratasich <denise.ratasich@tuwien.ac.at>
2016-09-13 11:02:29 +0200

    add verify-installation to install script

--- install_theme.sh
+++ install_theme.sh
@@ -4,6 +4,7 @@
 # @brief Installs the theme locally.
 # 
 # Local manual installation as described in:
+# https://en.wikibooks.org/wiki/LaTeX/Installing_Extra_Packages
 # http://tex.stackexchange.com/questions/1137/where-do-i-place-my-own-sty-or-cls-files-to-make-them-available-to-all-my-te
 ##
 
@@ -17,11 +18,20 @@ mkdir -p "${TEXDIR}"
 
 # install beamer theme
 if kpseaccess -w "${TEXDIR}"; then
+    echo "Copy files ..."
     mkdir -p "${TEXDIR}"/$THEMEDIR
     cp $THEMEDIR/beamerthemeTUW.sty \
-	$THEMEDIR/TU_Logo.pdf $THEMEDIR/TU_Signet.pdf \
-	"${TEXDIR}"/$THEMEDIR
+        $THEMEDIR/TU_Logo.pdf $THEMEDIR/TU_Signet.pdf \
+        "${TEXDIR}"/$THEMEDIR
     break
 fi
 
-echo "TUW theme installed to ${TEXDIR}/${THEMEDIR}."
+# verify installation
+path=$(kpsewhich beamerthemeTUW.sty) # search TUW beamer theme
+ret=$?
+if [ $ret -eq 0 ]; then
+    echo "TUW theme installed to ${TEXDIR}/${THEMEDIR}."
+else
+    echo "Installation failed. Cannot find beamerthemeTUW.sty."
+    exit 1
+fi
